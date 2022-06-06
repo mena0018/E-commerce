666d80c06c6d4bd6cccf1ba86701b90d65540e50
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-06-07 00:13:22 +0200

    Ajout du panier en haut à gauche

--- src/Components/FloatingCart/FloatingCard.js
+++ src/Components/FloatingCart/FloatingCard.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import cartIcon from "./shopping-cart.svg"
+import "./FloatingCart.scss"
+import { Link } from 'react-router-dom';
+
 
 export default function FloatingCard() {
+  
   return (
-    <div>FloatingCard</div>
+    <Link to="panier">
+      <div className='floating-cart'>
+
+        <p>Votre panier</p>
+
+        <div className="img-notif-container">
+          <img src={cartIcon} alt="icöne panier" />
+          <span>0</span>
+        </div>
+
+      </div>
+    </Link>
   )
 }
